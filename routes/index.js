656a61633aca4b6cc90c22618bda0ef5b5cc7b9e
AJKAR <karczewskiamanda@gmail.com>
2022-02-28 05:23:55 -0600

    complete lab

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ import { Router } from 'express'
 const router = Router()
 
 /* GET home page. */
+// localhost:3000
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Skills' })
 })
 
 export { 
---
+++ routes/skills.js
@@ -0,0 +1,24 @@
+import { Router } from 'express'
+import * as skillsCtrl from "../controllers/skills.js"
+const router = Router()
+
+/* GET users listing. */
+// GET - localhost:3000/todos
+router.get('/', skillsCtrl.index)
+// GET - localhost:3000/todos/new
+router.get("/new", skillsCtrl.new)
+// GET - localhost:3000/todos/:id
+router.get("/:id", skillsCtrl.show)
+
+// POST - localhost:3000/todos
+router.post("/", skillsCtrl.create)
+
+// DELETE - localhost:3000/todos/:id
+router.delete("/:id", skillsCtrl.delete)
+
+
+
+export {
+  router
+}
+
